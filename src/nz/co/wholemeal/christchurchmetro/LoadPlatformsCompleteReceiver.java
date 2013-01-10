44968ffc5def3ece659171bc5d1998643a3c3f23
Malcolm Locke <malc@wholemeal.co.nz>
2013-01-10 16:28:01 +1300

    Rm unused var from LoadPlatformsCompleteReceiver

--- src/nz/co/wholemeal/christchurchmetro/LoadPlatformsCompleteReceiver.java
+++ src/nz/co/wholemeal/christchurchmetro/LoadPlatformsCompleteReceiver.java
@@ -3,7 +3,6 @@ package nz.co.wholemeal.christchurchmetro;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -15,7 +14,6 @@ public class LoadPlatformsCompleteReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle extras = intent.getExtras();
-        Resources res = context.getResources();
         String message = extras.getString(MESSAGE);
 
         if (message == null) {
